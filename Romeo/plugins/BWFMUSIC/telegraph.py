1e18944691b566d71f06ccef08de61e05d3e1c9c
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-06-05 10:38:09 +0530

    Create telegraph.py

---
+++ Romeo/plugins/BWFMUSIC/telegraph.py
@@ -0,0 +1,31 @@
+from telegraph import upload_file
+from pyrogram import filters
+from Romeo import app
+from pyrogram.types import InputMediaPhoto
+
+### ❖ ➥ 𝗕𝐖𝗙™🇮🇳
+@app.on_message(filters.command(["tgm" , "photo" , "link" ,"telegraph"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("💌ʙω͠ғ™ ɪᴍᴀɢᴇs🦋")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://telegra.ph" + x
+
+        i.edit(f'💌ʙω͠ғ™ ʟɪɴᴋ ɪᴍᴀɢᴇs🦋 {url}')
+
+########____________________________________________________________######
+
+@app.on_message(filters.command(["graph" , "grf"]))
+def ul(_, message):
+    reply = message.reply_to_message
+    if reply.media:
+        i = message.reply("💌ʙω͠ғ™ ɪᴍᴀɢᴇs🦋")
+        path = reply.download()
+        fk = upload_file(path)
+        for x in fk:
+            url = "https://graph.org" + x
+
+        i.edit(f'💌ʙω͠ғ™ ʟɪɴᴋ ɪᴍᴀɢᴇs🦋 {url}')
