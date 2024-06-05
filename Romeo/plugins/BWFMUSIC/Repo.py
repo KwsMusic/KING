fe50c79149ea0faa04632106ace9195bdcfe6bc9
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-06-05 16:17:03 +0530

    Update Repo.py

--- Romeo/plugins/BWFMUSIC/Repo.py
+++ Romeo/plugins/BWFMUSIC/Repo.py
@@ -2,7 +2,7 @@ from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from Romeo import app as bot
 import requests
-from config import BOT_USERNAME
+from config import
 from Romeo.utils.errors import capture_err
 
 start_txt = """**
